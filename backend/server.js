59d3b82ed28cfe49377816bb8c860004ac16f41d
AndriyTinkalyuk <tinkalukandriy@gmail.com>
2025-05-26 01:10:03 +0300

    add cloude mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { app } = require("./app");
 
 mongoose
-	.connect("mongodb://127.0.0.1:27017/shapeshifter", {
+	.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
